e322fed5724a50808679d22182925b624f0f1096
Slava <slavasotnikov2020@gmail.com>
2021-03-26 14:24:48 +0200

    Added last task

--- Class2_15_03_21/Task2/Program.cs
+++ Class2_15_03_21/Task2/Program.cs
@@ -7,7 +7,10 @@ namespace Task2
         static void Main()
         {
             char[] charArr = new char[6];
-            int count = 1;
+            int count = 0;
+            int count1 = 0;
+            int count2 = 0;
+
             string sign = String.Empty;
             string index = String.Empty;
             string index1 = String.Empty;
@@ -16,24 +19,52 @@ namespace Task2
             charArr[1] = '}';
             charArr[2] = '[';
             charArr[3] = '[';
-            charArr[4] = ']';
+            charArr[4] = '(';
             charArr[5] = ']';
 
             for (int i = 0; i < charArr.Length; i++)
             {
-                for (int j = i + 1; j < charArr.Length; j++)
+                if (charArr[i] == '(')
                 {
-                    if (charArr[i] == charArr[j])
-                    {
-                        count++;
-                        sign += charArr[i];
-                        //index += j;
-                        //index1 += i;
-                    }
+                    count++;
+                }
+                if (charArr[i] == ')')
+                {
+                    count--;
+                }
+                if (charArr[i] == '{')
+                {
+                    count1++;
+                }
+                if (charArr[i] == '}')
+                {
+                    count1--;
+                }
+                if (charArr[i] == '[')
+                {
+                    count2++;
+                }
+                if (charArr[i] == ']')
+                {
+                    count2--;
                 }                
             }
-            Console.WriteLine("You have {0} similar signs {1}, index {2} {3}", count, sign, index, index1);
-
+            if (count == 0 && count1 == 0 && count2 == 0)
+            {
+                Console.WriteLine("Congrats! Your sentence is complete.");
+            }
+            if (count != 0)
+            {
+                Console.WriteLine($"You have to close {count} '(' brackets.");
+            }
+            if (count1 != 0)
+            {
+                Console.WriteLine("You have to close {0} '{' brackets.", count1);
+            }
+            if (count2 != 0)
+            {
+                Console.WriteLine($"You have to close {count2} '[' brackets.");
+            }
             Console.ReadLine();
         }
     }
