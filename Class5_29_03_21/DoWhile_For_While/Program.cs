495c36ea4026415ec53286986656195fbe7918a5
Slava <slavasotnikov2020@gmail.com>
2021-04-01 18:05:48 +0200

    Added all tasks

---
+++ Class5_29_03_21/DoWhile_For_While/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DoWhile_For_While
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = "It's a big deal!";
+            string output = String.Empty;
+
+            // For using While
+            /*
+            for (int i = 0; i < input.Length; i++)
+            {
+                 output += input[i];
+            }
+
+            int i = 0;
+            while (i < input.Length)
+            {
+                output += input[i];
+                i++;
+            }
+            */
+
+            //While using For
+
+            /*
+            while (input != "0")
+            {
+                 Console.WriteLine("while");
+            }
+
+            input = Console.ReadLine();
+            for (; input != "0"; )
+            {
+                Console.WriteLine("for");
+            }
+            */
+
+            //DoWhile using For
+
+
+            /*
+            for (int i = 0; i <= 0; i++)
+            {
+                Console.WriteLine("for");
+            }
+            */
+
+            //For using DoWhile 
+
+            /*
+            int i = 0;
+            do
+            {
+                output += input[i];
+                i++;
+            } while (i < input.Length);
+            */
+
+            //DoWhile using While
+
+            // We can use <= or >= with While cycle. In these cases we have a one cycle, then false and quite.
+
+            //Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
--- Class5_29_03_21/Min_Max/Program.cs
+++ Class5_29_03_21/Min_Max/Program.cs
@@ -6,6 +6,136 @@ namespace Min_Max
     {
         static void Main()
         {
+            /*
+            // While()
+
+            int number = 0; //Do I have to initialize this variable, or I can declare the variable like this?
+          //int number;       Why? Why not?
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            int max = number;
+            int min = number;
+
+            while (number != -1) // How can I quit the program avoid numbers?
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+                if (number < min)
+                {
+                    min = number;
+                }
+
+                Console.Write("Max: {0}  Min: {1}   ", max, min);
+
+                number = int.Parse(Console.ReadLine());
+            }
+            */
+
+            // Do While()
+
+            /*
+            int number = 0;
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            int max = number;
+            int min = number;
+
+            do
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+                if (number < min)
+                {
+                    min = number;
+                }
+
+                Console.Write("Max: {0}  Min: {1}   ", max, min);
+
+                number = int.Parse(Console.ReadLine());
+
+            } while (number != -1);
+            */
+
+            // For(;;)
+
+            /*
+            int number = 0;
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            int max = number;
+            int min = number;
+
+            for ( ; number != -1; )
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+                if (number < min)
+                {
+                    min = number;
+                }
+
+                Console.Write("Max: {0}  Min: {1}   ", max, min);
+
+                number = int.Parse(Console.ReadLine());
+            }
+            */
+
+            //It's all for nothing.
+
+            /*
+            string number = String.Empty;
+            string numberStr = String.Empty;
+            int numberInt = 0;
+
+            Console.Write("Enter a number: ");
+            numberStr = Console.ReadLine();
+
+            int max = Convert.ToInt32(numberStr);
+            int min = Convert.ToInt32(numberStr);
+
+            for (int i = 0; i < numberStr.Length; i++)
+            {
+                number += Convert.ToString(numberStr[i] - '0');
+                numberInt = Convert.ToInt32(number);
+            }
+           
+
+            for (; numberStr.ToLower() != "q";)
+            {
+                if (numberInt > max)
+                {
+                    max = numberInt;
+                }
+                if (numberInt < min)
+                {
+                    min = numberInt;
+                }
+
+                Console.Write("Max: {0}  Min: {1}   ", max, min);
+
+                numberStr = Console.ReadLine();
+                number = String.Empty;
+                for (int i = 0; i < numberStr.Length; i++)
+                {
+                    
+                    number += Convert.ToString(numberStr[i] - '0');
+                    numberInt = Convert.ToInt32(number);
+                }
+                
+            }
+            */
         }
     }
 }
