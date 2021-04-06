bff8c3c8b49be41e680332753a198a87df4a0364
Slava <slavasotnikov2020@gmail.com>
2021-04-06 17:50:40 +0200

    Fixed Shift and Addition

--- Class5_29_03_21/Shift_and_Addition/Program.cs
+++ Class5_29_03_21/Shift_and_Addition/Program.cs
@@ -3,50 +3,38 @@
 namespace Shift_and_Addition
 {
     class Program
-    {       
+    {
+        static ushort GetPowerOfTwo(ushort multiplier1, ref ushort shift)
+        {
+            shift = 0;
+            ushort rate = 0x01;
+            while (rate <= multiplier1)
+            {
+                rate = (ushort)(rate << 1);
+                shift++;
+            }
+            shift--;
+            return (ushort)(rate >> 1);
+        }
         static void Main()
         {
-            int multiplier1 = 125;
-            int multiplier2 = 17;
-            int product = 0;
+            ushort multiplier1 = 5;
+            int multiplier2 = 2;
+            int result = 0;
+            ushort shift = 0;
+            ushort balance = 0;
 
-            if (64 <= multiplier1 && multiplier1 < 128)
-            {
-                multiplier1 -= 64;
-                product += multiplier2 << 6;
-            }
-            if (32 <= multiplier1 && multiplier1 < 64)
-            {
-                multiplier1 -= 32;
-                product += multiplier2 << 5;
-            }
-            if (16 <= multiplier1 && multiplier1 < 32)
-            {
-                multiplier1 -= 16;
-                product += multiplier2 << 4;
-            }
-            if (8 <= multiplier1 && multiplier1 < 16)
-            {
-                multiplier1 -= 8;
-                product += multiplier2 << 3;
-            }
-            if (4 <= multiplier1 && multiplier1 < 8)
-            {
-                multiplier1 -= 4;
-                product += multiplier2 << 2;
-            }
-            if (2 <= multiplier1 && multiplier1 < 4)
-            {
-                multiplier1 -= 2;
-                product += multiplier2 << 1;
-            }
-            if (0 <= multiplier1 && multiplier1 < 2)
-            {
-                multiplier1 -= 0;
-                product += multiplier2 << 0;
-            }
+            balance = (ushort)(multiplier1 - GetPowerOfTwo(multiplier1, ref shift));
+            result = multiplier2 << shift;
 
-            Console.WriteLine(product);
+            while (balance > 0)
+            {
+                balance -= GetPowerOfTwo(balance, ref shift);
+                result += multiplier2 << shift;
+            }          
+
+            Console.WriteLine("Shift and Addition: {0}", result);
+            Console.WriteLine("Multiplication:     {0}", multiplier1 * multiplier2);
             Console.ReadKey();
         }
     }
