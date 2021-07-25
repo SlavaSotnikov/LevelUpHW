fb9acb62c3c45e42d2ad393c2a693fe227ec9f44
Slava <slavasotnikov2020@gmail.com>
2021-07-25 18:34:03 +0200

    Fixed the names of objects.

--- Class17_2021_07_23/GeometricShapes/Program.cs
+++ Class17_2021_07_23/GeometricShapes/Program.cs
@@ -7,17 +7,13 @@ namespace GeometricShapes
         static void Main()
         {
             //Point one = new Point(5, 1);
-            //Console.ForegroundColor = ConsoleColor.Red;
             //UI.ShowPoint(one);
-            //Console.ResetColor();
 
-            //Line two = new Line(15, 20, 30, 40);
+            //Line straight = new Line(15, 20, 30, 40);
             //UI.ShowLine(two);
 
-
-
-            Triangle three = new Triangle(5, 10, 30, 30, 5, 30);
-            UI.ShowTriangle(three);
+            Triangle right = new Triangle(5, 10, 30, 30, 5, 30);
+            UI.ShowTriangle(right);
 
             Console.ReadKey();
         }
