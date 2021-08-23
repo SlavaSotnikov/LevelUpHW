c84257bb8610d2af0e45969c1403a136247a0b66
slavasotnikov2020@gmail.com <slavasotnikov2020@gmail.com>
2021-08-23 18:37:14 +0300

    very first implementation of Queue

--- Class18_2021_08_21/Queue/Program.cs
+++ Class18_2021_08_21/Queue/Program.cs
@@ -8,14 +8,15 @@ namespace Queue
         {
             Queue one = new Queue(3);
 
-            one.Add(1);
-            one.Add(2);
-            one.Add(3);
+            one.Add("one");
+            one.Add("two");
+            one.Add("three");
 
-            one.Get();
+            Console.WriteLine(one.Get());
+            Console.WriteLine(one.Get());
 
-            one.Add(4);
-            one.Add(5);
+            one.Add("four");
+            one.Add("five");
 
             Console.ReadKey();
         }
