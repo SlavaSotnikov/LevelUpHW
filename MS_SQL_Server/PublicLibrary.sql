533300cddf2b9ec78a8359ecdd6a6c31a0d183ce
Slava <slavasotnikov2020@gmail.com>
2021-12-15 18:43:57 +0200

    Filled in tables

--- MS_SQL_Server/PublicLibrary.sql
+++ MS_SQL_Server/PublicLibrary.sql
@@ -4,6 +4,8 @@ GO
 --DROP DATABASE PublicLibrary
 --GO
 
+SELECT * FROM BooksOperation
+
 CREATE DATABASE PublicLibrary
 GO
 
@@ -24,15 +26,15 @@ CREATE TABLE Writers
 	FirstName  NCHAR(20)     NOT NULL,
 	LastName   NCHAR(20)     NOT NULL,
 	MiddleName NCHAR(20)     NULL,
-	DeletedOn  SMALLDATETIME NULL,
-	WhoRemoved BIGINT        NULL
+	Country    VARCHAR(20)   NULL
 )
 
-CREATE TABLE ProcessBooks
+CREATE TABLE BooksOperation
 (
     ReaderId   BIGINT        NOT NULL,
-	BookId     BIGINT        NOT NULL,
+	CopyId     BIGINT        NOT NULL,
 	Given      DATE          NOT NULL,
+	WhoGiven   BIGINT        NOT NULL,
 	Back       DATE          NULL
 )
 
@@ -49,15 +51,13 @@ CREATE TABLE Staff
 	LastName   NCHAR(20)     NOT NULL,
 	MiddleName NCHAR(20)     NULL,
 	Haired     DATE          NOT NULL,
-	Faired     DATE          NULL,
-	DeletedOn  SMALLDATETIME NULL,
-	WhoRemoved BIGINT        NULL
+	Faired     DATE          NULL
 )
 
 CREATE TABLE WorkerPosition
 (
-    WorkerId   BIGINT NOT NULL,
-	PositionId BIGINT NOT NULL
+    WorkerId   BIGINT        NOT NULL,
+	PositionId BIGINT        NOT NULL
 )
 
 CREATE TABLE Occupation
@@ -66,10 +66,11 @@ CREATE TABLE Occupation
 	Position   NCHAR(50)     NOT NULL
 )
 
-CREATE TABLE BookAmount
+CREATE TABLE BookCopy
 (
-    BookId BIGINT            NOT NULL,
-	Amount INT               NOT NULL
+    CopyId       BIGINT      NOT NULL IDENTITY (1, 1),
+	BookId       BIGINT      NOT NULL,
+	Condition    TINYINT     NOT NULL
 )
 
 CREATE TABLE Readers
@@ -81,6 +82,8 @@ CREATE TABLE Readers
 	SubscribeDate   DATE         NOT NULL,
 	UnsubscribeDate DATE         NULL
 )
+
+GO
 
 ALTER TABLE Readers 
     ADD CONSTRAINT PK_Readers
@@ -102,14 +105,36 @@ ALTER TABLE Staff
     ADD CONSTRAINT PK_Worker
 	    PRIMARY KEY CLUSTERED (WorkerId)
 
-ALTER TABLE ProcessBooks
+ALTER TABLE BookCopy 
+    ADD CONSTRAINT PK_BookCopy
+	    PRIMARY KEY CLUSTERED (CopyId)
+
+ALTER TABLE WorkerPosition
+    ADD CONSTRAINT FK_Worker_Worker
+	    FOREIGN KEY(WorkerId) REFERENCES Staff(WorkerId)
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION
+
+ALTER TABLE WorkerPosition
+    ADD CONSTRAINT FK_Worker_Position
+	    FOREIGN KEY(PositionId) REFERENCES Occupation(PositionId)
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION
+
+ALTER TABLE BooksOperation
     ADD CONSTRAINT FK_Books_Readers
 	    FOREIGN KEY(ReaderId) REFERENCES Readers(ReaderId)
 		ON UPDATE CASCADE
 		ON DELETE NO ACTION
 
-ALTER TABLE ProcessBooks
-    ADD CONSTRAINT FK_BookId_Books
+ALTER TABLE BooksOperation
+    ADD CONSTRAINT FK_Books_Copy
+	    FOREIGN KEY(CopyId) REFERENCES BookCopy(CopyId)
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION
+
+ALTER TABLE BookCopy
+    ADD CONSTRAINT FK_BookCopy_Book
 	    FOREIGN KEY(BookId) REFERENCES Books(BookId)
 		ON UPDATE CASCADE
 		ON DELETE NO ACTION
@@ -126,27 +151,15 @@ ALTER TABLE BooksWriters
 		ON UPDATE CASCADE
 		ON DELETE NO ACTION
 
-ALTER TABLE ProcessBooks
-    ADD CONSTRAINT FK_Reader_Books
-	    FOREIGN KEY(ReaderId) REFERENCES Books(BookId)
+ALTER TABLE Books
+    ADD CONSTRAINT FK_Book_Remove
+	    FOREIGN KEY(WhoRemoved) REFERENCES Staff(WorkerId)
 		ON UPDATE NO ACTION
 		ON DELETE NO ACTION
 
-ALTER TABLE BookAmount
-    ADD CONSTRAINT FK_Books_Amount
-	    FOREIGN KEY(BookId) REFERENCES Books(BookId)
-		ON UPDATE CASCADE
-		ON DELETE NO ACTION
-
-ALTER TABLE WorkerPosition
-    ADD CONSTRAINT FK_Worker_Worker
-	    FOREIGN KEY(WorkerId) REFERENCES Staff(WorkerId)
-		ON UPDATE CASCADE
-		ON DELETE NO ACTION
-
-ALTER TABLE WorkerPosition
-    ADD CONSTRAINT FK_Worker_Position
-	    FOREIGN KEY(PositionId) REFERENCES Occupation(PositionId)
+ALTER TABLE BooksOperation
+    ADD CONSTRAINT FK_Book_Given
+	    FOREIGN KEY(WhoGiven) REFERENCES Staff(WorkerId)
 		ON UPDATE CASCADE
 		ON DELETE NO ACTION
 
@@ -164,24 +177,50 @@ BACKUP DATABASE PublicLibrary
 TO DISK = 'D:\SQLBackups\PublicLibraryDB.bak'
    WITH FORMAT,
       MEDIANAME = 'SQLServerBackups',
-      NAME = 'Full Backup of PublicLibraryDB';
+      NAME = 'Full Backup of PublicLibraryDB'; --Try GETDATE
+	  --CONVERT(VARCHAR, datetime [,style])
+	  --CONCAT(string_value1, string_value2 [, string_valueN ]);
 GO
 
-INSERT INTO Writers(FirstName, LastName, MiddleName)
-    VALUES('William', 'Shakespeare', NULL),
-	      ('Jane', 'Austen', NULL),
-	      ('George', 'Orwell', NULL),
-	      ('Thomas', 'Hardy', NULL),
-	      ('Thomas', 'Eliot', 'Stearns'),
-	      ('William', 'Blake', NULL),
-	      ('Lewis', 'Caroll', NULL),
-	      ('Lev', 'Tolstoy', 'Nikolaevich'),
-	      ('Fyodor', 'Dostoevsky', 'Mikhailovich'),
-	      ('Nikolai', 'Gogol', 'Vasilyevich'),
-	      ('Anton', 'Chekhov', 'Pavlovich'),
-	      ('Izabella', 'Akhmadulina', 'Akhatovna'),
-	      ('Ilya', 'Ilf', NULL),
-	      ('Yevgeny', 'Petrov', NULL)
+INSERT INTO Staff(FirstName, LastName, MiddleName, Haired, Faired)
+    VALUES('Vasil', 'Klein', NULL,'2020-01-01', NULL),
+	      ('Den', 'Fridel', NULL,'2020-02-02', NULL),
+		  ('Bob', 'Brown', NULL,'2020-03-03', NULL),
+		  ('Anna', 'Mateo', NULL,'2020-04-04', NULL)
+
+INSERT INTO Occupation(Position)
+    VALUES('Consultant'),
+	      ('Librarian'),
+		  ('DataBaseAdmin'),
+		  ('CareTaker'),
+		  ('Security')
+
+INSERT INTO WorkerPosition(WorkerId, PositionId)
+    VALUES(1, 1),
+	      (1, 2),
+		  (2, 3),
+		  (3, 4),
+		  (3, 5),
+		  (4, 2)
+GO
+
+INSERT INTO Writers(FirstName, LastName, MiddleName, Country)
+    VALUES('William', 'Shakespeare', NULL, 'U.K.'),
+	      ('Jane', 'Austen', NULL, 'U.K.'),
+	      ('George', 'Orwell', NULL, 'U.K.'),
+	      ('Thomas', 'Hardy', NULL, 'U.K.'),
+	      ('Thomas', 'Eliot', 'Stearns', 'U.K.'),
+	      ('William', 'Blake', NULL, 'U.K.'),
+	      ('Lewis', 'Caroll', NULL, 'U.K.'),
+	      ('Lev', 'Tolstoy', 'Nikolaevich', 'Russian Empire'),
+	      ('Fyodor', 'Dostoevsky', 'Mikhailovich', 'Russian Empire'),
+	      ('Nikolai', 'Gogol', 'Vasilyevich', 'Russian Empire'),
+	      ('Anton', 'Chekhov', 'Pavlovich', 'Russian Empire'),
+	      ('Izabella', 'Akhmadulina', 'Akhatovna', 'Russian Federation'),
+	      ('Ilya', 'Ilf', NULL, 'Sovet Union'),
+	      ('Yevgeny', 'Petrov', NULL, 'Sovet Union'),
+		  ('Alexey', 'Tolstoy', 'Konstantinovich', 'Russian Empire'),
+		  ('Alexey', 'Tolstoy', 'Nikolaevich', 'Russian Empire')
 GO
 
 INSERT INTO Books(Title)
@@ -212,6 +251,35 @@ INSERT INTO Books(Title)
 	      ('The Little Golden Calf')
 GO
 
+INSERT INTO BooksWriters(BookId, AuthorId)
+    VALUES(1, 1),
+	      (2, 1),
+	      (3, 1),
+		  (4, 2),
+		  (5, 2),
+		  (6, 3),
+		  (7, 4),
+		  (8, 4),
+		  (9, 5),
+		  (10, 5),
+		  (11, 6),
+		  (12, 6),
+		  (13, 7),
+		  (14, 7),
+		  (15, 8),
+		  (16, 8),
+		  (17, 9),
+		  (18, 9),
+		  (19, 10),
+		  (20, 10),
+		  (21, 11),
+		  (22, 11),
+		  (23, 12),
+		  (24, 12),
+		  (25, 13),
+		  (25, 14)
+GO
+
 INSERT INTO Readers(FirstName, LastName, MiddleName, SubscribeDate, UnsubscribeDate)
      	VALUES(N'Rick', N'Bor',   NULL,   '2021-09-08', NULL),
 		      (N'Bob', N'Arum',   NULL,   '2021-04-05', NULL),
@@ -223,52 +291,121 @@ INSERT INTO Readers(FirstName, LastName, MiddleName, SubscribeDate, UnsubscribeD
 		      (N'Adam', N'Tur',   NULL, '2021-12-08', NULL)
 GO
 
-INSERT INTO ProcessBooks(ReaderId, BookId, Given, Back)
-    VALUES(2, 1, '2021-10-11', NULL),
-	      (2, 2, '2021-10-11', NULL),
-	      (6, 10, '2021-12-08', NULL),
-	      (6, 4, '2021-09-08', '2021-10-03'),
-	      (8, 9, '2021-09-03', NULL),
-	      (8, 8, '2021-04-08', '2021-05-08'),
-	      (5, 7, '2021-10-08', NULL)
+INSERT INTO BookCopy(BookId, Condition)
+     VALUES(1, 10),
+	       (1, 10),
+	       (1, 10),
+	       (1, 10),
+	       (2, 10),
+	       (2, 5),
+	       (2, 9),
+	       (2, 1),
+	       (3, 3),
+	       (3, 8),
+	       (4, 10),
+	       (4, 6),
+	       (5, 10),
+	       (5, 8),
+	       (5, 2),
+		   (6, 2),
+		   (6, 10),
+		   (7, 2),
+		   (7, 10),
+		   (8, 1),
+		   (8, 2),
+		   (8, 3),
+		   (8, 9),
+		   (9, 2),
+		   (9, 10),
+		   (9, 7),
+		   (9, 3),
+		   (10, 2),
+		   (10, 7),
+		   (10, 8),
+		   (10, 2),
+		   (10, 2),
+		   (11, 10),
+		   (11, 10),
+		   (11, 10),
+		   (11, 10),
+		   (11, 10),
+		   (12, 9),
+		   (12, 9),
+		   (12, 9),
+		   (12, 9),
+		   (13, 8),
+		   (13, 7),
+		   (13, 6),
+		   (13, 5),
+		   (14, 10),
+		   (15, 8),
+		   (16, 9),
+		   (17, 10),
+		   (17, 10),
+		   (17, 10),
+		   (17, 10),
+		   (18, 9),
+		   (19, 1),
+		   (19, 0),
+		   (19, 0),
+		   (19, 0),
+		   (19, 1),
+		   (20, 0),
+		   (20, 10),
+		   (20, 7)
 GO
 
-INSERT INTO BookAmount(BookId, Amount)
-    VALUES(1, 4),
-	(2, 4),
-	(3, 2),
-	(4, 7),
-	(5, 2),
-	(6, 3),
-	(7, 5),
-	(8, 3),
-	(9, 6)
+INSERT INTO BooksOperation(ReaderId, CopyId, Given, WhoGiven, Back)
+    VALUES(1, 1, '2021-09-08', 1 , NULL),
+	      (2, 2, '2021-10-11', 1,  NULL),
+	      (6, 10, '2021-12-08', 2, NULL),
+	      (6, 4, '2021-09-08', 3 ,'2021-10-03'),
+	      (8, 9, '2021-09-03', 2,  NULL),
+	      (8, 8, '2021-04-08', 2 ,'2021-05-08'),
+	      (5, 7, '2021-10-08', 3,  NULL),
+		  (6, 7, '2021-10-09', 4,  NULL),
+		  (7, 7, '2021-10-10', 3,  NULL),
+		  (8, 7, '2021-10-11', 3,  NULL),
+		  (8, 12, '2021-10-11', 3,  NULL),
+		  (8, 13, '2021-10-11', 3,  NULL)
 GO
 
-INSERT INTO BooksWriters(BookId, AuthorId)
-    VALUES(1, 1),
-	      (2, 1),
-	      (3, 1)
-GO
 
-INSERT INTO Staff(FirstName, LastName, MiddleName, Haired, Faired, DeletedOn)
-    VALUES('Vasil', 'Klein', NULL,'2020-01-01', NULL, NULL),
-	      ('Den', 'Fridel', NULL,'2020-02-02', NULL, NULL),
-		  ('Bob', 'Brown', NULL,'2020-03-03', NULL, NULL)
 
-INSERT INTO Occupation(Position)
-    VALUES('Consultant'),
-	      ('Librarian'),
-		  ('DataBaseAdmin'),
-		  ('CareTaker'),
-		  ('Security')
 
-INSERT INTO WorkerPosition(WorkerId, PositionId)
-    VALUES(1, 1),
-	      (1, 2),
-		  (2, 3),
-		  (3, 4),
-		  (3, 5)
-GO
+SELECT WriterId, FirstName, LastName, MiddleName, Country
+FROM Writers
 
-SELECT * FROM WorkerPosition
+SELECT LastName AS RussianWriters
+FROM Writers
+    WHERE (Country LIKE 'Russian%') OR
+          (Country LIKE 'Sovet%')
+
+SELECT WriterId AS BritishWriters
+FROM Writers
+    WHERE (FirstName = 'Thomas') AND
+	      (Country = 'U.K.')
+
+SELECT LastName AS BritishWriters
+FROM Writers
+    WHERE Country = 'U.K.'
+	ORDER BY LastName
+
+
+
+SELECT Title, DeletedOn, WhoRemoved
+FROM Books
+
+SELECT WhoRemoved, DeletedOn
+FROM Books
+    WHERE DeletedOn IS NOT NULL
+
+SELECT Title
+FROM Books
+    WHERE Title LIKE 'a%'
+
+SELECT Title
+FROM Books
+    WHERE Title LIKE 'v_%'
+
+
